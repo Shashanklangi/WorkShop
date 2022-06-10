79d989f7b4e89b00e1ae029b8c6e1f299645db72
Shashank <langishashank@gmail.com>
2022-06-10 10:01:13 +0530

    [Shashank]:power

---
+++ Work/Work/Power.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    public class PowerOf2
+    {
+        public void PowerMethod()
+        {
+            Console.WriteLine("Enter the Number");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            while (N > 0 && N < 31)
+            {
+                Console.WriteLine("Enter the Correct Number");
+                N = Convert.ToInt32(Console.ReadLine());
+            }
+            if (N > 0 && N < 31)
+            {
+                for (int i = 0; i < N; i++)
+                    Console.WriteLine(2 + " ^ " + i + " = " + Math.Pow(2, i));
+            }
+
+        }
+    }
+}
---
+++ Work/Work/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Workshop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PowerOf2 power = new PowerOf2();
+            power.PowerMethod();
+        }
+    }
+}
